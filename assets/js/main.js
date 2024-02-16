803c4271adc984adee90c82aaa58c9b7d22b199e
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-16 14:30:46 +0100

    add exercise 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@
 const number = 100; //number
 // Print the value of the variable to the console.
 console.log(number);
+
+// Exercise 2
+// Declare a constant PI and assign it the value 3.14. 
+const PI = 3.14; //number
+// Print the value of the constant to the console.
+console.log(PI);
