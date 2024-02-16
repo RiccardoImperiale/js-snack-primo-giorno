6f2145b2858374bac9acfe3c30484d2faf11af09
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-16 14:29:02 +0100

    add exercise 1

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+// Exercise 1
+// Declare a variable called number and assign it the value 100. 
+const number = 100; //number
+// Print the value of the variable to the console.
+console.log(number);
